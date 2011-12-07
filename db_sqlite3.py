c20e34ce5a2d7fc82ec68b695d07a9228565df88
laucliff <clifford.lau@utoronto.ca>
2011-12-06 21:57:38 -0500

    Finished db test case, wrapped up db_sqlite3

--- db_sqlite3.py
+++ db_sqlite3.py
@@ -2,93 +2,98 @@ import sqlite3
 
 class db_sql3:
 
-  def ___init___(params):
+  def __init__(self, params):
 
     #For performance reasons, db functions will blindly assume that the connection is open, instead of open/closing every time an operation is required.
     if 'location' in params:
-      open(params['location'])
+      self.open_db(params['location'])
     else:
-      open()
+      self.open_db('default.db')
 
     #Check if {clean: true} is provided as a param.
     if 'clean' in params and params['clean']:
       #reset tables.
-      c.execute('''DROP TABLE IF EXISTS words''')
-      c.execute('''DROP TABLE IF EXISTS pages''')
-      c.execute('''DROP TABLE IF EXISTS links''')
-      c.execute('''DROP TABLE IF EXISTS indices''')
+      self.c.execute('''DROP TABLE IF EXISTS words''')
+      self.c.execute('''DROP TABLE IF EXISTS pages''')
+      self.c.execute('''DROP TABLE IF EXISTS links''')
+      self.c.execute('''DROP TABLE IF EXISTS indices''')
       #I am not sure if the commit here is necessary, but I want to ensure that all the drops are performed before recreating the tables.
-      c.commit
+      self.conn.commit()
       #create tables
-      c.execute('''CREATE TABLE words (word TEXT UNIQUE;''')
-      c.execute('''CREATE TABLE pages (url TEXT UNIQUE, rank INTEGER);''')
-      c.execute('''CREATE TABLE links (parent INTEGER, child INTEGER, PRIMARY KEY(parent, child));''')
-      c.execute('''CREATE TABLE indices (word_id INTEGER, url_id INTEGER, size INTEGER, count INTEGER DEFAULT 0, PRIMARY KEY(word_id, url_id, size));''')
-      c.commit()
+      self.c.execute('''CREATE TABLE words (word TEXT UNIQUE);''')  
+      self.c.execute('''CREATE TABLE pages (url TEXT UNIQUE, rank INTEGER DEFAULT 0);''')
+      self.c.execute('''CREATE TABLE links (parent INTEGER, child INTEGER, PRIMARY KEY(parent, child));''')
+      self.c.execute('''CREATE TABLE indices (word_id INTEGER, url_id INTEGER, size INTEGER, count INTEGER DEFAULT 0, PRIMARY KEY(word_id, url_id, size));''')
+      self.conn.commit()
 
-  def open(self, db_location = 'database'):
+  def open_db(self, db_location):
     self.conn = sqlite3.connect(db_location)
-    self.c = conn.cursor()
+    self.conn.text_factory = str
+    self.c = self.conn.cursor()
 
-  def close():
-    c.close()
-    conn.close()
+  def close_db(self):
+    self.c.close()
+    self.conn.close()
 
-  def addWord(word):
+  def addWord(self, word):
     #INSERT OR IGNORE INTO words VALUES (word);
-    c.execute('''INSERT OR IGNORE INTO words VALUES (?);''', word)
-    conn.commit()
+    self.c.execute('''INSERT OR IGNORE INTO words (word) VALUES (?);''', (word,))
+    self.conn.commit()
 
-  def addPage(url):
+  def addPage(self, url):
     #INSERT OR IGNORE INTO pages VALUES (url);
-    c.execute('''INSERT OR IGNORE INTO pages VALUES (?);''', url)
-    conn.commit()
+    self.c.execute('''INSERT OR IGNORE INTO pages (url) VALUES (?);''', (url,))
+    self.conn.commit()
 
-  def addLink(parent, child):
+  def addLink(self, parent, child):
     #INSERT OR IGNORE INTO links VALUES (parent, child);
     #python suggest we use the ? replacement instead of normal string formatting in order to prevent sql injection attacks. However, it does not seem to support key value linking like normal string formatting. As a result, some of the string substitution can be a bit messy.
     #query = 'INSERT OR IGNORE INTO links VALUES (%(parent)s, %(child)s);' % {'parent':parent, 'child':child}
-    c.execute('''INSERT OR IGNORE INTO links VALUES (?,?);''', (parent, child))
-    conn.commit()
+    
+    self.c.execute('''SELECT rowid FROM pages WHERE url=?;''', (parent,))
+    parent_id = next(iter(self.c.fetchone()))
+    self.c.execute('''SELECT rowid FROM pages WHERE url=?;''', (child,))
+    child_id = next(iter(self.c.fetchone()))
 
-  def addIndex(word, url, size):
+    self.c.execute('''INSERT OR IGNORE INTO links (parent, child) VALUES (?,?);''', (parent_id, child_id))
+    self.conn.commit()
+
+  def addIndex(self, word, url, size):
     #sqlite3 lacks a proper insert or update on conflict query, so we have to do his messy setup instead.
     #if index exists, increment count value.
     #insert or update into indices select words.rowid, pages.rowid from words left join pages where words like 'word' and url like 'url';
-    c.execute('''SELECT rowid FROM words WHERE word LIKE '?';''', word)
-    word_id = c
-    c.execute('''SELECT rowid FROM pages WHERE url LIKE '?';''', url)
-    url_id = c
+    self.c.execute('''SELECT rowid FROM words WHERE word=?;''', (word,))
+    word_id = next(iter(self.c.fetchone()))
+    self.c.execute('''SELECT rowid FROM pages WHERE url=?;''', (url,))
+    url_id = next(iter(self.c.fetchone()))
 
-
-
-    c.execute('''INSERT OR IGNORE INTO indices (word_id, url_id, size) VALUES (?, ?, ?);''', (word_id, url_id, size))
-    c.commit()
-    c.execute('''UPDATE indices set count = count+1 WHERE word_id = ? AND url_id = ?''', (word_id, url_id))
-    c.commit()
+    self.c.execute('''INSERT OR IGNORE INTO indices (word_id, url_id, size) VALUES (?,?,?);''', (word_id, url_id, size))
+    self.conn.commit()
+    self.c.execute('''UPDATE indices set count = count+1 WHERE word_id = ? AND url_id = ?''', (word_id, url_id))
+    self.conn.commit()
 
   def doSearch(word):
-    c.execute('''SELECT word_id, url_id, size, count FROM indices WHERE word like '?';''', word)
-    return c
+    self.c.execute('''SELECT word_id, url_id, size, count FROM indices WHERE word like '?';''', (word,))
+    return self.c
 
-  def getAllIndices():
+  def getAllIndices(self):
     #SELECT * FROM indices
-    c.execute('''SELECT * FROM indices;''')
-    return c
+    self.c.execute('''SELECT * FROM indices order by rowid;''')
+    return self.c.fetchall()
 
-  def updatePageRank(url, rank):
+  def updatePageRank(self,url, rank):
     #update table pages set rank = rank where url = url
     #UPDATE pages SET rank = rank WHERE url = url;
     #query = 'UPDATE pages SET rank = %(rank)s WHERE url = %(url)s;' % {'rank':rank, 'url':url}
-    c.execute('''UPDATE pages SET rank = ? WHERE url like '?';''', (rank, url))
-    c.commit()
+    self.c.execute('''UPDATE pages SET rank = ? WHERE url like '?';''', (rank, url))
+    self.conn.commit()
 
-  def customQuery(query):
-    #For direct query execution
-    c.execute('''?''', query)
-    c.commit()
-    return c
+  def customQuery(self,query):
+    #For direct query execution. Returns the raw cursor object. Mostly used for testing.
+    self.c.execute(query)
+    self.conn.commit()
+    return self.c
 
-  def sanitize(query):
+  def sanitize(self,query):
     return
     #sanitize the input to prevent syntax errors/injection attacks
--- test_db_sqlite3.py
+++ test_db_sqlite3.py
@@ -1,15 +1,113 @@
-import db_sqlite3
+import db_sqlite3 as db_lib
 import unittest
 
 class TestSqlite3Functions(unittest.TestCase):
 
-  addWord = ('word1', 'word2', 'word1', 'word1')
-  addWordAnswer = set('word1', 'word2') 
+  #TODO doSearch, once properly implemented, and sanitize, once I work that out.
 
-  db = db_sqlite3({'clean': True, 'location': 'testdb'})
+  def setUp(self):
+    self.db = db_lib.db_sql3({'clean': True, 'location': 'test.db'})
 
-  for word in addWord:
-    db.addWord(addWord)
-  
-  db.customQuery('select * from words')
-  
+  def test_addWord(self):
+    pass
+    #Test addWord function, and unique enforcement. and proper store.
+    db = self.db
+
+    db.addWord('word1')
+    results = db.customQuery('select * from words').fetchall()
+    self.assertEqual(results, [('word1',)])
+
+    db.addWord('word1')
+    results = db.customQuery('select * from words').fetchall()
+    self.assertEqual(results, [('word1',)])
+
+    db.addWord('word2')
+    results = db.customQuery('select * from words order by rowid').fetchall()
+    self.assertEqual(results, [('word1', ),('word2',)])
+
+  def test_addPage(self):
+    pass
+    #Functionally identical to test_addWord
+    db = self.db
+
+    db.addPage('url1')
+    results = db.customQuery('select url from pages').fetchall()
+    self.assertEqual(results, [('url1',)])
+
+    db.addPage('url1')
+    results = db.customQuery('select url from pages').fetchall()
+    self.assertEqual(results, [('url1',)])
+
+    db.addPage('url2')
+    results = db.customQuery('select url from pages order by rowid').fetchall()
+    self.assertEqual(results, [('url1',), ('url2',)])
+
+
+  def test_addLink(self):
+    pass
+    #Test basic addLink functionality, and composite primary key values
+    db = self.db
+
+    db.addPage('url1')
+    db.addPage('url2')
+    db.addPage('url3')
+    db.addPage('url4')
+
+    db.addLink('url1', 'url2')
+    results = db.customQuery('select parent, child from links order by rowid').fetchall()
+    self.assertEqual(results, [(1,2)])
+
+    db.addLink('url1', 'url2')
+    results = db.customQuery('select parent, child from links order by rowid').fetchall()
+    self.assertEqual(results, [(1,2)])
+    
+    db.addLink('url1', 'url3')
+    results = db.customQuery('select parent, child from links order by rowid').fetchall()
+    self.assertEqual(results, [(1,2),(1,3)])
+
+    db.addLink('url3', 'url4')
+    results = db.customQuery('select parent, child from links order by rowid').fetchall()
+    self.assertEqual(results, [(1,2),(1,3),(3,4)])
+
+
+  def test_Index(self):
+    #Test addIndex, getAllIndices
+    db = self.db
+
+    db.addWord('word1')
+    db.addWord('word2')
+    db.addPage('url1')
+    db.addPage('url2')
+
+    db.addIndex('word1', 'url1', 1)
+    results = db.customQuery('select word_id, url_id, size, count from indices order by rowid').fetchall()
+    self.assertEqual(results, [(1, 1, 1, 1)])
+
+    db.addIndex('word1', 'url1', 1)
+    results = db.customQuery('select word_id, url_id, size, count from indices order by rowid').fetchall()
+    self.assertEqual(results, [(1, 1, 1, 2)])
+    
+    #Last two queries should be identical
+
+    db.addIndex('word2', 'url2', 1)
+    results = db.customQuery('select word_id, url_id, size, count from indices order by rowid').fetchall()
+    self.assertEqual(results, [(1, 1, 1, 2), (2, 2, 1, 1)])
+
+    
+    results = db.getAllIndices()
+    self.assertEqual(results, [(1, 1, 1, 2), (2, 2, 1, 1)])
+
+    def test_updateRank(self):
+      #Tests update rank.
+      db = self.db
+
+      db.addPage('url1')
+
+      db.updateRank('url1', 5)
+      results = db.customQuery('select url, rank from pages').fetchall()
+      self.assertEqual(results, [(1, 1, 5)])
+
+
+
+if __name__ == '__main__' :
+  unittest.main()
